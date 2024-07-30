79d3044e50ea84548b36f2c250b9d1f182bdf71b
MohammadAmmad <117718535+Ammadgithub123@users.noreply.github.com>
2024-07-30 12:15:48 +0500

    Initial commit

---
+++ Middleware/Middleware/Program.cs
@@ -0,0 +1,42 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("AMMAD and Moiz \n");
+    await next(context);
+});
+
+app.Use (async (context, next) =>
+{
+    await context.Response.WriteAsync("Sir Areeb \n");
+    await next(context);
+});
+
+app.Run(async (context) =>
+{
+    await context.Response.WriteAsync("HELLO WORLD");
+});
+
+app.Run();
